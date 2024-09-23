b27b72753659ec9823844436ecb3ea3d0748e6ca
DiegodelosV <diegodelosv@gmail.com>
2024-09-23 14:05:40 -0300

    Inicialización Proyecto

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "desafio-crud-3fa79.firebaseapp.com",
+  projectId: "desafio-crud-3fa79",
+  storageBucket: "desafio-crud-3fa79.appspot.com",
+  messagingSenderId: "1025335899887",
+  appId: "1:1025335899887:web:725223e4ca6ec4bcfb0d1a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
